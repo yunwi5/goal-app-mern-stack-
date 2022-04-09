606869265b1330e7a4e35e92526011010dc9ab63
Yunkeun <1009jyk@gmail.com>
2022-04-10 01:02:19 +1200

    Ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,33 @@
-const path = require('path');
-const express = require('express');
-const colors = require('colors');
-const cors = require('cors');
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-const { errorHandler } = require('./middleware/error-middleware');
-const connectDB = require('./config/db');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/error-middleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
 
-const app = express();
-connectDB();
+connectDB()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const app = express()
 
-app.use('/api/goals', require('./routes/goal-routes'));
-app.use('/api/users', require('./routes/user-routes'));
-app.use(errorHandler);
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// Server frontend
+app.use('/api/goals', require('./routes/goal-routes'))
+app.use('/api/users', require('./routes/user-routes'))
+
+// Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')),
-    );
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
 } else {
-    app.get('/', (req, res) => res.send('Please set to production'));
+    app.get('/', (req, res) => res.send('Please set to production'))
 }
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`App serving on the port ${PORT}`);
-});
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
